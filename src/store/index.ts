75f0b9f398bca065c7b8130259fe6c0058e48444
diswho <discover.who@gmail.com>
2023-10-06 13:56:27 +0700

    0610

---
+++ src/store/index.ts
@@ -0,0 +1,5 @@
+import Vue from 'vue';
+import Vuex, { StoreOptions } from 'vuex';
+
+import { mainModule } from './main';
+import { State } from './state';
---
+++ src/views/main/Start.vue
@@ -0,0 +1,9 @@
+<template>
+    <router-view></router-view>
+</template>
+  
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+</script>
+
+<style></style>
